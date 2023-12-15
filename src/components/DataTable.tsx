131bb8198a5b1eee9c25ecc46997b3e004b33c3c
Yaphet Abraha <yaphet.abraha@gmail.com>
2023-12-15 08:50:04 -0600

    add pagination to data table

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -146,7 +146,7 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      {/* <div className="flex-1 text-sm text-muted-foreground">
+      <div className="flex-1 text-sm text-muted-foreground">
         {table.getFilteredSelectedRowModel().rows.length} of{" "}
         {table.getFilteredRowModel().rows.length} row(s) selected.
       </div>
@@ -167,7 +167,7 @@ export function DataTable<TData, TValue>({
         >
           Next
         </Button>
-      </div> */}
+      </div>
     </>
   );
 }
